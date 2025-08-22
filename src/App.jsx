d12df3d5cac726997347ca2198e22e119e3e2c82
Diogo Logrado <diogo.logrado@wedigitek.com>
2025-08-22 09:09:52 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div style={{ textAlign: "center", margin: "auto auto" }}>
+      <h1>Keepsakes 👋</h1>
+    </div>
+  )
+}
+
+export default App
